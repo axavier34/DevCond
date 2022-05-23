dc5f1630cef662166bca86037f12c73a9b5a0457
Alex Xavier Rosa <axavier34@gmail.com>
2022-05-23 15:05:48 -0300

    Created Routes, Register, Login, Logout in the controller AuthController and Seeds to populate databases

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::post('/auth/login',[AuthController::class, 'login']);
 Route::post('/auth/register',[AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function(){
+    
     Route::post('/auth/validate',[AuthController::class, 'validateToken']);
     Route::post('/auth/logout',[AuthController::class, 'logout']);
 
